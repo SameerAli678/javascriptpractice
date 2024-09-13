a6e3fbe6914b16ff91a7a56bb3d981d5a7644c38
SameerAli678 <alikhansameer@gmail.com>
2024-09-13 15:25:59 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+//  let marks = {
+//     sameerali :100,
+//     forkan : 20,
+//     mursaleen : 5
+// } 
+// //  console.log( marks);
+
+//    for (let  i = 0; i < Object.keys(marks).lenght; i++) {
+//           console.log("the" + Object.keys(marks )[i] + "are" + marks[ Object.keys(marks )[i]]);
+//         }
+
+
+
+ let marks = {
+    sameerali: 100,
+    forkan: 20,
+    mursaleen: 5
+};
+
+for (let i = 0 ; i < Object.keys(marks).length; i++) {
+    console.log("The " + Object.keys(marks)[i] + " has " + marks[Object.keys(marks)[i]] + " marks");
+}
+
+//     let a =" sa,iudbqwuyb"
+//   console.log(a);
+// i = prompt( "enter a roll number ")
+
+//   let s = 13
+//   let i
+//   while (i =s) {
+//     console.log("try again");
+   
+    
+// }
+//   console.log("correct roll");  
+let  num  = [1,2,3,4,5,6,7,8,9,10] 
+//  console.log(roll_of_class_10 
+// console.log(roll_10 [8]);
+// console.log(roll_10 [9]);
+
+
+
+//  for (let i = 0; i < roll_10.length; i++) {
+//   console.log(roll_10 [i]);
+  
+  
+//  }
+  let n = num.toString(
+   
+    
+
+  )
+  console.log(n)
+  console.log( num ,typeof  num);
+  let j = num.join (" and " )
+  console.log(j);
+  // let pop = num.pop( )
+  // console.log(num, pop); 
+  // let push = num.push (56)
+  // console.log(num, push);
+  // let shift = num.shift( )
+  // console.log(num, shift);
+  let unshift =num.unshift (78)
+  console.log(num, unshift);
+  
+  
+  
+  
+  
+  
+   
+  
+  
