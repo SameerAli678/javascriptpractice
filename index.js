9d95a7dc7811dd0d933c93ee1d31faa8c7dde802
SameerAli678 <alikhansameer@gmail.com>
2024-09-15 14:44:53 -0700

    map and filter and reduse vedio no 20

--- index.js
+++ index.js
@@ -11,15 +11,19 @@
 
 
 
- let marks = {
-    sameerali: 100,
-    forkan: 20,
-    mursaleen: 5
-};
+//  let marks = {
+//     sameerali: 100,
+//     forkan: 20,
+//     mursaleen: 5
+// };
+// console.log(marks);
+// console.log(typeof marks );
 
-for (let i = 0 ; i < Object.keys(marks).length; i++) {
-    console.log("The " + Object.keys(marks)[i] + " has " + marks[Object.keys(marks)[i]] + " marks");
-}
+
+
+// for (let i = 0 ; i < Object.keys(marks).length; i++) {
+//     console.log("The " + Object.keys(marks)[i] + " has " + marks[Object.keys(marks)[i]] + " marks");
+// }
 
 //     let a =" sa,iudbqwuyb"
 //   console.log(a);
@@ -33,7 +37,7 @@ for (let i = 0 ; i < Object.keys(marks).length; i++) {
     
 // }
 //   console.log("correct roll");  
-let  num  = [1,2,3,4,5,6,7,8,9,10] 
+// let  num  = [1,2,3,4,5,6,7,8,9,10] 
 //  console.log(roll_of_class_10 
 // console.log(roll_10 [8]);
 // console.log(roll_10 [9]);
@@ -45,24 +49,100 @@ let  num  = [1,2,3,4,5,6,7,8,9,10]
   
   
 //  }
-  let n = num.toString(
+//   let n = num.toString(
    
     
 
-  )
-  console.log(n)
-  console.log( num ,typeof  num);
-  let j = num.join (" and " )
-  console.log(j);
-  // let pop = num.pop( )
-  // console.log(num, pop); 
-  // let push = num.push (56)
-  // console.log(num, push);
+//   )
+//   console.log(n)
+//   console.log( num ,typeof  num);
+//   let j = num.join (" and " )
+//   console.log(j);
+  // let pop = num.pop( ) 
+  // console.log(num, pop);  
+  // let push = num.push (56) 
+  // console.log(num, push); 
   // let shift = num.shift( )
-  // console.log(num, shift);
-  let unshift =num.unshift (78)
-  console.log(num, unshift);
+  // console.log(num, shift); 
+//   let unshift =num.unshift (78) 
+//   console.log(num, unshift); 
+
+// let  num  = [1,2,3,4,5,6,7,8,9] 
+// let num2 = [11,12,13,14,15,16,17,18,19]
+//  let num3 = [21,22,23,24,25,26,27,28,29,30]
+//    delete num[0 ]
+// console.log(num);
+// let newarray = num.concat(num2 ,num3)
+
+// console.log(newarray);
+// console.log(num, num2 );
+// let  num1  = [1,2,3,4,5,6,7,8,9]
+// let compere = (a,b )=> {
+//     return (b-a )
+// }
+// num.sort( compere)
+// //  console.log(num);
+//  num.reverse()
+//  console.log(num1);
+//  num.splice(2 ,4,35 ,45 ,55, 65,) 
+//  console.log(num);
+// let newnum = num1.slice (2)
+// console.log(newnum);
+// let num2 = [ 1 , 2 , 3 , 4]
+// for (let i = 0; i < num2.length; i++) {
+//   console.log(num2[i] );
+  // }
+// num2.forEach((Element) => {
+  // console.log(Element * Element);
   
+// })
+// console.log(num2);
+
+// let name  =  "sameerali"
+//   let arr = Array.from(name)
+//   console.log(arr); 
+//    for (let  i of num2)
+//   console.log(i);
+//    for (let  i in num2)
+//     console.log(i);
+
+
+   let sam = [3, 4, 5, 6 ]
+  //  console.log(sam);
+  //  let a = sam.map ( (value ,index , arrey)=> {
+
+  //   console.log(value ,index , arrey);
+  //   return value +1;
+    
+  //  })
+  //  console.log(a); 
+    //  let a = sam.filter((value  )=>{
+    //   return value <= 5
+    //    })
+
+    //  console.log(a);
+     let a = sam.reduce ( (h1 , h2 )=>{
+      return h1 & h2 
+     }
+
+     )
+     console.log(a);
+     
+   
+   
+   
+   
+   
+
+      
+  
+    
+
+
+ 
+
+   
+     
   
   
   
