5296d1f9a60d8569f4750850d763b9b30c50c6ec
SameerAli678 <alikhansameer@gmail.com>
2024-09-19 13:50:40 -0700

    28 video complete ,dom end bom

--- index.js
+++ index.js
@@ -2,14 +2,12 @@
 //     sameerali :100,
 //     forkan : 20,
 //     mursaleen : 5
-// } 
+// }
 // //  console.log( marks);
 
 //    for (let  i = 0; i < Object.keys(marks).lenght; i++) {
 //           console.log("the" + Object.keys(marks )[i] + "are" + marks[ Object.keys(marks )[i]]);
 //         }
-
-
 
 //  let marks = {
 //     sameerali: 100,
@@ -18,8 +16,6 @@
 // };
 // console.log(marks);
 // console.log(typeof marks );
-
-
 
 // for (let i = 0 ; i < Object.keys(marks).length; i++) {
 //     console.log("The " + Object.keys(marks)[i] + " has " + marks[Object.keys(marks)[i]] + " marks");
@@ -33,41 +29,35 @@
 //   let i
 //   while (i =s) {
 //     console.log("try again");
-   
-    
+
 // }
-//   console.log("correct roll");  
-// let  num  = [1,2,3,4,5,6,7,8,9,10] 
-//  console.log(roll_of_class_10 
+//   console.log("correct roll");
+// let  num  = [1,2,3,4,5,6,7,8,9,10]
+//  console.log(roll_of_class_10
 // console.log(roll_10 [8]);
 // console.log(roll_10 [9]);
 
-
-
 //  for (let i = 0; i < roll_10.length; i++) {
 //   console.log(roll_10 [i]);
-  
-  
+
 //  }
 //   let n = num.toString(
-   
-    
 
 //   )
 //   console.log(n)
 //   console.log( num ,typeof  num);
 //   let j = num.join (" and " )
 //   console.log(j);
-  // let pop = num.pop( ) 
-  // console.log(num, pop);  
-  // let push = num.push (56) 
-  // console.log(num, push); 
-  // let shift = num.shift( )
-  // console.log(num, shift); 
-//   let unshift =num.unshift (78) 
-//   console.log(num, unshift); 
+// let pop = num.pop( )
+// console.log(num, pop);
+// let push = num.push (56)
+// console.log(num, push);
+// let shift = num.shift( )
+// console.log(num, shift);
+//   let unshift =num.unshift (78)
+//   console.log(num, unshift);
 
-// let  num  = [1,2,3,4,5,6,7,8,9] 
+// let  num  = [1,2,3,4,5,6,7,8,9]
 // let num2 = [11,12,13,14,15,16,17,18,19]
 //  let num3 = [21,22,23,24,25,26,27,28,29,30]
 //    delete num[0 ]
@@ -84,70 +74,83 @@
 // //  console.log(num);
 //  num.reverse()
 //  console.log(num1);
-//  num.splice(2 ,4,35 ,45 ,55, 65,) 
+//  num.splice(2 ,4,35 ,45 ,55, 65,)
 //  console.log(num);
 // let newnum = num1.slice (2)
 // console.log(newnum);
 // let num2 = [ 1 , 2 , 3 , 4]
 // for (let i = 0; i < num2.length; i++) {
 //   console.log(num2[i] );
-  // }
+// }
 // num2.forEach((Element) => {
-  // console.log(Element * Element);
-  
+// console.log(Element * Element);
+
 // })
 // console.log(num2);
 
 // let name  =  "sameerali"
 //   let arr = Array.from(name)
-//   console.log(arr); 
+//   console.log(arr);
 //    for (let  i of num2)
 //   console.log(i);
 //    for (let  i in num2)
 //     console.log(i);
 
+//  let sam = [3, 4, 5, 6 ]
+//  console.log(sam);
+//  let a = sam.map ( (value )=> {
 
-   let sam = [3, 4, 5, 6 ]
-  //  console.log(sam);
-  //  let a = sam.map ( (value ,index , arrey)=> {
+// console.log(value );
+// return value + 1;
 
-  //   console.log(value ,index , arrey);
-  //   return value +1;
-    
-  //  })
-  //  console.log(a); 
-    //  let a = sam.filter((value  )=>{
-    //   return value <= 5
-    //    })
+//  })
+//  console.log(a);
+//  let a = sam.filter((value  )=>{
+//   return value <= 5
+//    })
 
-    //  console.log(a);
-     let a = sam.reduce ( (h1 , h2 )=>{
-      return h1 & h2 
-     }
+//  console.log(a);
+//  let a = sam.reduce ( (h1 , h2 )=>{
+// return h1 + h2
+//  }
 
-     )
-     console.log(a);
-     
-   
-   
-   
-   
-   
+//  )
+//  console.log(a);
+//  let number = Math.round(Math.random() *10)
+//  console.log(number);
+// let number = Math.round(Math.random() * 100);
+// let number = 19
+// console.log(number);
 
-      
+// let sam = prompt(" what is your roll number ");
+// sam = Number.parseInt(sam);
+
+// if (number === sam) {
+  // alert("true");
+// } else {
+  // alert("try again because right asnwer is " + number);
+  // window.location.reload();
+// }
+
+// let write = confirm("write" )
+// if (write ){
+//   console.log("thanks");
   
-    
+// }else{
+//   console.log("click ok ");
+  
+// }
+// console.log(window);
+// console.log(document);
+document.body.style.background ="yellow" 
+let a  = 5
+let b = 6
 
 
- 
 
-   
-     
-  
-  
-  
-  
-  
-   
-  
-  
+
+
+console.log(a+b );
+
+
+
